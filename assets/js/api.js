ccf516afdde605ab1c1f0fb6c6a50145334efe46
LucasEpifanio <Lucasepifaniodev@gmail.com>
2023-08-07 13:37:33 +0100

    clima-tempo

---
+++ assets/js/api.js
@@ -0,0 +1,45 @@
+/**
+ * @license MIT
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @copyright lucasepifanio 2023 All rights reserved
+ * @author codewithsadee <lucasepifaniodev@gmail.com>
+ */
+
+'use strict';
+
+const api_key = "c96ab88b67dbce0416c8c8ee061b634b";
+
+/**
+ *  
+  *@param {string} URL
+  *@param {function} callback
+  */
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+
+    /** 
+  *@param {string} query
+  */
+
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
